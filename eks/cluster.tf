34427ae8e13d574322d25560f7d9f9bfb69b4a5d
AshikR <ashikravindran24@gmail.com>
2026-05-30 12:14:04 +0530

    created eks nodegroup policy

---
+++ eks/cluster.tf
@@ -0,0 +1,19 @@
+resource "aws_eks_cluster" "main" {
+  name     = "microservices-demo"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  access_config {
+    authentication_mode = "API"
+  }
+
+  vpc_config {
+    subnet_ids = [
+      data.terraform_remote_state.vpc.outputs.public_subnet_1_id,
+      data.terraform_remote_state.vpc.outputs.public_subnet_2_id
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_policy
+  ]
+}
